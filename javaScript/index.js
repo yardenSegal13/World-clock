1d92de5a9e1c1d33457ee1e627c384480837f560
Yarden Segal <yarden.segal@gmail.com>
2023-04-04 14:04:51 +0200

    Added the live time  data of selected locations

--- javaScript/index.js
+++ javaScript/index.js
@@ -1,12 +1,48 @@
-function update() {
+function showCity() {
   let newYorkElement = document.querySelector("#new-york");
-  let newYorkTime = newYorkElement.querySelector(".time");
-  newYorkTime.innerHTML = moment()
-    .tz("America/New_York")
-    .format("h:mm:ss[<small>]A[</small>]");
-  let newYorkDate = newYorkElement.querySelector(".date");
-  newYorkDate.innerHTML = moment().tz("America/New_York").format(`D MMMM YYYY`);
+  if (newYorkElement) {
+    let newYorkTime = newYorkElement.querySelector(".time");
+    newYorkTime.innerHTML = moment()
+      .tz("America/New_York")
+      .format("h:mm:ss[<small>]A[</small>]");
+    let newYorkDate = newYorkElement.querySelector(".date");
+    newYorkDate.innerHTML = moment()
+      .tz("America/New_York")
+      .format(`D MMMM YYYY`);
+  }
 }
 
-update();
-setInterval(update, 1000);
+function changeCity(event) {
+  let cityTimeZone = event.target.value;
+
+  if (cityTimeZone.length > 0) {
+    let cityTime = moment().tz(cityTimeZone);
+    let city = cityTimeZone.replace("_", " ").split("/")[1];
+    let cityBlock = document.querySelector("#cities");
+    cityBlock.innerHTML = `<div class="city">
+        <div class="date-block">
+          <h2>${city}</h2>
+          <div class="date">${cityTime.format("D MMMM YYYY")}</div>
+        </div>
+        <div class="time">${cityTime.format("h:mm:ss")}<small>${cityTime.format(
+      "A"
+    )}</small></div></div>
+     `;
+  } else {
+    let cityBlock = document.querySelector("#cities");
+    cityBlock.innerHTML = `<div class="city">
+        <div class="date-block">
+          <h2>Please select a city 😉</h2>
+          <div class="date"></div>
+        </div>
+        <div class="time"><small>
+       
+    </small></div></div>
+     `;
+  }
+}
+
+let selectCityElement = document.querySelector("#select-city");
+selectCityElement.addEventListener("change", changeCity);
+showCity();
+setInterval(showCity, 1000);
